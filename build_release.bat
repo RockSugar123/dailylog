d1ba4f8607b755ef78da096fc470c3cc05ffa152
GSFU <2874399416@qq.com>
2026-09-06 00:09:51 +0800

    chore: bump installer version to 1.2.0（问答历史会话）

--- build_release.bat
+++ build_release.bat
@@ -5,4 +5,4 @@ call "%~dp0build.bat"
 if errorlevel 1 ( echo PYINSTALLER FAILED & exit /b 1 )
 "C:\Program Files (x86)\Inno Setup 6\iscc.exe" installer.iss
 if errorlevel 1 ( echo ISCC FAILED & exit /b 1 )
-echo DONE: dist\dailylog-setup-1.0.0.exe
+echo DONE: dist\dailylog-setup-1.2.0.exe
